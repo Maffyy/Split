c34f70eebba1b8fd09d652b60230f882e1b2c577
Maffyy <martin.studna2@gmail.com>
2018-04-10 16:37:26 +0200

    efewf

--- Split/Source.cpp
+++ Split/Source.cpp
@@ -8,16 +8,19 @@
 #include <functional>
 
 
-template<typename T1, typename ... TList>
+template<typename ... T>
 class Pack {
 public:
-	std::istringstream stream;
+	std::istream stream;
+	std::tuple<T...> tuple;
+	Pack(std::tuple<T ...> && tup) : tup(tuple) {};
 };
 
-template<typename T1,typename ... TList>
-class Pack<T1,TList ...>  {
+template<typename ... T>
+class Pack<T...>  {
 public:
-	int a;
+	std::istream stream;
+
 };
 
 template<typename T>
@@ -28,23 +31,10 @@ void process_rvalue(T& elem) {
 }
 template<typename T>
 void process_lvalue(T& elem) {
+
 	if (std::is_lvalue_reference(elem)) {
 
-	}//fewofjweoif
-}
-
-
-template<typename ... T>
-Pack<T ...> split(T && ... p) {
-	//using tuple1 = std:tuple<std::remove_reference_t<T> ... >;
-	using tuple2 = std::tuple<T ...>;
-	using tuple3 = std::tuple<T && ...>;
-
-	
-	auto data1 = std::make_tuple(std::forward<T>(p)...);
-	parse(data1);
-	auto data2 = std::tie(p ...);
-	parse(data2);
+	}
 }
 
 template<std::size_t I, typename ... T>
@@ -52,8 +42,20 @@ void parse(std::tuple<T...> & p) {
 
 }
 
-template<typename ... TL>
-std::istream& operator>>(std::istream& is,Pack<TL ...> && pack) {
+
+template<typename ... T>
+Pack<T ...> split(T && ... params) {
+	std::tuple<T ...>tuple(std::forward<T>(params) ...);
+	parse<sizeof(tuple),T...>(tuple);
+	Pack<T...> pack(tuple);
+
+	return pack;
+}
+
+
+
+template<typename ... T>
+std::istream& operator>>(std::istream& is,Pack<T ...> && pack) {
 	is >> pack.stream;
 	return is;
 }
